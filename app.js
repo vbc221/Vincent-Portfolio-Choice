0baf8f07f6cbf40ff7511d012125d053d14a31db
vbc221 <vbc221@nyu.edu>
2019-09-24 09:53:57 -0500

    sidebar mostlye done

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 //changes icon blue to what is selected
 
-let icon = document.querySelectorAll(".fas");
-
-let home=document.querySelector('.fa-home');
-for (let i = 0; i <icon.length; i++) {
-  icon[i].addEventListener("click", function (event) {
-    //makes sure it stop refreshing
-    //overrides last blue color
-    icon[i].className= icon[i].className.replace('active', '');
-    this.add('active');
-  });
-}
-
 
