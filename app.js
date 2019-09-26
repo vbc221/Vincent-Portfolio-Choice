98a59648a45765b32d27e14fcdd1a6ea43691da1
vbc221 <vbc221@nyu.edu>
2019-09-26 01:55:54 -0500

    finally figured out active

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 //changes icon blue to what is selected
 
+// let icons=document.querySelectorAll('.fas');
 
+// for (i = 0; i < icons.length; i++) {
+
+//   icons[i].addEventListener("click", function () {
+
+//     for (i = 0; i < icons.length; i++) {
+//       icons[i].classList.remove('active');
+//     }
+
+//     this.classList.add("active");
+
+//   });
+
+// }
